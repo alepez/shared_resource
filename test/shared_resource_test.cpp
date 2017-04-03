302aaed5abcb54b8864d7a8ec68984290f37ecb3
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2017-04-03 14:50:34 +0200

    exclusive lock

--- test/shared_resource_test.cpp
+++ test/shared_resource_test.cpp
@@ -1,3 +1,4 @@
+#include <boost/thread/shared_mutex.hpp>
 #include <gtest/gtest.h>
 #include <memory>
 
@@ -5,19 +6,42 @@ template <typename T> class SharedResource {
 public:
 	using ResourceType = T;
 
+	class ExclusiveLock {
+		ExclusiveLock(boost::shared_mutex* mutex)
+				: mutex_{mutex} {
+			mutex_->lock();
+		}
+		~ExclusiveLock() {
+			mutex_->unlock();
+		}
+		boost::shared_mutex* mutex_;
+	};
+
 	template <typename... Args>
 	SharedResource(Args&&... args)
 			: resource_{new T(std::forward<Args>(args)...)} {
 	}
 
+	ExclusiveLock exclusiveLock() {
+		return ExclusiveLock{&mutex_};
+	}
+
+	SharedResource() = default;
+	SharedResource& operator=(SharedResource&&) = default;
+
 private:
 	std::unique_ptr<T> resource_;
+	boost::shared_mutex mutex_;
 };
 
-TEST(SharedResourceTest, Init) {
-	auto that = SharedResource<std::string>();
+TEST(SharedResourceTest, InitAsInt) {
+	SharedResource<int> that;
+}
+
+TEST(SharedResourceTest, InitAsString) {
+	SharedResource<std::string> that;
 }
 
 TEST(SharedResourceTest, InitWithArgs) {
-	auto that = SharedResource<std::string>("ciao");
+	SharedResource<std::string> that("ciao");
 }
